15ec5e9ce168356abf449c195bc574b211d23cb3
Milton Amaya <blurrdinfo@Gmail.com>
2024-10-07 11:26:49 -0700

    Update gatsby-config.js and add new plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,12 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-console.log("Space ID:", process.env.CONTENTFUL_SPACE_ID)
-console.log("Access Token:", process.env.CONTENTFUL_ACCESS_TOKEN)
-
 module.exports = {
   siteMetadata: {
     title: `blurrd`,
     description: `A starter template for Gatsby with Contentful integration.`,
     author: `@yourusername`,
+    siteUrl: `https://blurrd.xyz`, // Add this line with your actual domain
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -27,6 +25,19 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-react-helmet`, // Add this for better SEO handling
+    {
+      resolve: `gatsby-plugin-manifest`, // Add this for PWA support
+      options: {
+        name: `blurrd`,
+        short_name: `blurrd`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site. Ensure this file exists or change the path.
       },
     },
   ],
